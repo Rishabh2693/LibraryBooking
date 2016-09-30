2ef9c179e365fd74fd0a3e7397778027e9806886
bkasliwal <bkasliwal@ncsu.edu>
2016-09-30 19:03:32 -0400

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = {:host => 'mighty-tundra-19161.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "library.ncstate@gmail.com",
+      :password             => "wonderwoman",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
 end
